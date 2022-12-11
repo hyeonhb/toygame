1a2f5fbb04ba3bd3c6fa310ce0fea1e869125b86
hyeonhb <hyeonnhb@gmail.com>
2022-12-11 14:46:09 +0900

    base settings

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <register-name />
+</template>
+
+<script>
+import RegisterName from './views/RegisterName.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RegisterName,
+  }
+}
+</script>
